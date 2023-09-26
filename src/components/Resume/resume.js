7b83e9a62bdb99be2ef21cffd59239a59e9ecf3f
archit203 <archit241001@gmail.com>
2023-09-26 13:57:45 +0530

    contact form added

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './resume.css'
 import Myresume from "../../../src/Archit's resume.pdf"
+import FacebookIcon from "../../assets/facebook-icon.png"
+import YoutubeIcon from "../../assets/youtube.png"
+import twitterIcon from "../../assets/twitter.png"
+import InstagramIcon from "../../assets/instagram.png"
 const Resume = () => {
   return (
     <section id='resume'>
@@ -8,6 +12,22 @@ const Resume = () => {
         <span className='resumeDesc'>Here is my resume, which showcases my experience , skills, certifications and achievements.</span>
         <div className='resumeDoc'>
             <iframe src={Myresume} className='resumeFrame'></iframe>
+        </div>
+        <div id='contact'>
+          <h1 className='contactPageTitle'>Contact Me</h1>
+          <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
+          <form action='' className='contactForm'>
+            <input type='text' className='name' placeholder='Your Name'/>
+            <input type='email' className='email' placeholder='Your Email'/>
+            <textarea name='message' rows={5} placeholder='Your Message' className='msg'></textarea>
+            <button className='submitBtn' type='submit' value='send'>submit</button>
+            <div className='links'>
+              <img src={FacebookIcon} alt='FacebookIcon' className='link'/>
+              <img src={YoutubeIcon} alt='YoutubeIcon' className='link'/>
+              <img src={twitterIcon} alt='twitterIcon' className='link'/>
+              <img src={InstagramIcon} alt='InstagramIcon' className='link'/>
+            </div>
+          </form>
         </div>
     </section>
   )
